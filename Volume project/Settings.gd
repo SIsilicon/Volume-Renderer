6318410a66aa1dccc5ac2d9fc22fb2a18f4957cf
Roujel Williams <sisilicon28@gmail.com>
2018-06-01 19:04:34 -0500

    Update to Version 0.3

--- Volume project/Settings.gd
+++ Volume project/Settings.gd
@@ -2,12 +2,19 @@ extends VBoxContainer
 
 onready var volume = $"../../Volume"
 
+func _process(delta):
+	$"../../FrameRate".text = str(Engine.get_frames_per_second())
+
 func _slice_count_changed(value):
 	volume.set_slices(value)
 	$SliceNumber/Label2.text = str(value)
 
-func _shaded_changed(button_pressed):
-	volume.set_shaded(button_pressed)
+func _dither_strength_changed(value):
+	volume.set_dither(value)
+	$DitherStrength/Label2.text = str(value)
+
+func _blend_mode_changed(item_selected):
+	volume.set_blend(item_selected)
 
 func _colour_changed(color):
 	volume.set_modulate(color)
